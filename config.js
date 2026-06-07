c45ea8974043f2b5549d72567653370ec0454eb1
kuapilangkenceng <kuapilangkenceng@gmail.com>
2026-06-07 09:06:24 +0700

    Update config.js
    Fase 7 - Fix doGet

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ const KUA = {
 
     // Ganti dengan URL deploy Apps Script Anda
     // Format: https://script.google.com/macros/s/XXXX/exec
-    api    : "https://script.google.com/macros/s/AKfycbxBz3tHZWX6vWvVmeYoagtSImqrytE_hTyVbCeWh3TlnKB5p1TsntGLD5UlO3Nm5spd/exec",  // [WAJIB DIISI]
+    api    : "https://script.google.com/macros/s/AKfycbwKEc2eRIAlvHywdGjC6RxSbjHQgIAZ5ATRvopLZuvqghy1AqXsRRkvO5SoisXTu7qR/exec",  // [WAJIB DIISI]
 
     // ID Google Sheets utama (ada di URL spreadsheet)
     sheetsId: "17TPFJ32oaWfqe4zW9uY8bPG1vfs56vOxr9g4DDJV_aY",            // [WAJIB DIISI]
